8b94c3e72a27009ca7006bcfedc7662640960cfe
scriptkiddie <karthikuttan@gmail.com>
2016-07-17 21:17:51 +0530

    Update views

--- rcomp/views.py
+++ rcomp/views.py
@@ -1,23 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from .check_sequence import check_sequence, reverse_complement, rtst
+from .check_sequence import check_sequence, reverse_complement, handle_uploaded_file
+import io
 
 # Create your views here.
 
 
 def index(request):
     if request.method == 'POST':
-        seq = request.POST.get('seq', False)
-        request.session['val'] = check_sequence(seq)
-        if seq:
-            return HttpResponseRedirect(reverse('rcomp:result'))
+        if request.POST['seq']:
+            seq = request.POST['seq']
+            seq = check_sequence(seq)
+            # saving the value of seq in 'val' to be passed between views
+            request.session['val'] = seq
+        elif request.FILES:
+            # ufile is InMemoryUploadedFile object, we can not read its
+            # contents as strings directly
+            ufile = request.FILES['upload_flle']
+            # InMemoryUploadedFile as a file object that is in BytesIO, so convert it to string
+            content = io.TextIOWrapper(request.FILES['upload_flle'].file)
+            seq = handle_uploaded_file(ufile, content)
+            request.session['val'] = seq
         else:
-            return render(request, 'rcomp/index.html')
+            return render(request, 'revcomp/index.html')
+        if seq:
+                return HttpResponseRedirect(reverse('revcomp:rcomp'))
+        else:
+            return render(request, 'revcomp/index.html')
     else:
-        return render(request, 'rcomp/index.html')
+        return render(request, 'revcomp/index.html')
 
 
-def result(request):
+def rcomp(request):
     myval = request.session['val']
-    return render(request, 'rcomp/results.html', {'seq': myval})
+    return render(request, 'revcomp/results.html', {'seq': myval})
