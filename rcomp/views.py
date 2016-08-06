9ea82e14bfb4d288425e7a5a3e399a1b65278cc5
scriptkiddie <karthikuttan@gmail.com>
2016-08-06 14:54:47 +0530

    Handle django-forms

--- rcomp/views.py
+++ rcomp/views.py
@@ -1,37 +1,37 @@
+from __future__ import absolute_import
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from .check_sequence import check_sequence, reverse_complement, handle_uploaded_file
 import io
+from .forms import FastaForm
 
-# Create your views here.
 
+def home(request):
+    return render(request, 'rcomp/home.html')
 
 def index(request):
     if request.method == 'POST':
-        if request.POST['seq']:
-            seq = request.POST['seq']
-            seq = check_sequence(seq)
-            # saving the value of seq in 'val' to be passed between views
-            request.session['val'] = seq
-        elif request.FILES:
-            # ufile is InMemoryUploadedFile object, we can not read its
-            # contents as strings directly
-            ufile = request.FILES['upload_flle']
-            # InMemoryUploadedFile as a file object that is in BytesIO, so convert it to string
-            content = io.TextIOWrapper(request.FILES['upload_flle'].file)
-            seq = handle_uploaded_file(ufile, content)
-            request.session['val'] = seq
-        else:
-            return render(request, 'revcomp/index.html')
-        if seq:
-                return HttpResponseRedirect(reverse('revcomp:rcomp'))
-        else:
-            return render(request, 'revcomp/index.html')
+        form = FastaForm(request.POST, request.FILES)
+        if form.is_valid():
+            if request.FILES:
+                ufile = request.FILES['fastafile']
+                content = io.TextIOWrapper(request.FILES['fastafile'].file)
+                seq = handle_uploaded_file(ufile, content)
+                request.session['val'] = seq
+                return HttpResponseRedirect(reverse('rcomp:result'))
+            elif request.POST['seq']:
+                seq = form.cleaned_data['seq']
+                seq = check_sequence(seq)
+                request.session['val'] = seq
+                return HttpResponseRedirect(reverse('rcomp:result'))
+            else:
+                return render(request, 'rcomp/index.html', {'form': form})
     else:
-        return render(request, 'revcomp/index.html')
+        form = FastaForm()
+        return render(request, 'rcomp/index.html', {'form': form})
 
 
-def rcomp(request):
+def result(request):
     myval = request.session['val']
-    return render(request, 'revcomp/results.html', {'seq': myval})
+    return render(request, 'rcomp/results.html', {'seq': myval})
